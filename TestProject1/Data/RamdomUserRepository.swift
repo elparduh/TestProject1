4c89a87f0c0c0eff6c37b74658d7098ec5a9df20
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-07-17 22:14:29 -0600

    IOS-02-Create-Data-Layer - Data (#2)

---
+++ TestProject1/Data/RamdomUserRepository.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+protocol RamdomUserRepositoryProtocol {
+  func getRamdomUser(completion: @escaping (Result<RamdomUserModel,DataTransferError>) -> Void)
+}
+
+struct RamdomUserRepository: RamdomUserRepositoryProtocol {
+    
+    private var ramdomUserAPIDataSource : RamdomUserAPIDataSourceProtocol
+
+    init(ramdomUserAPIDataSource : RamdomUserAPIDataSourceProtocol){
+      self.ramdomUserAPIDataSource = ramdomUserAPIDataSource
+    }
+    
+    func getRamdomUser(completion: @escaping (Result<RamdomUserModel, DataTransferError>) -> Void) {
+        ramdomUserAPIDataSource.getRamdomUser { resultData  in
+            completion(resultData)
+        }
+    }
+    
+}
