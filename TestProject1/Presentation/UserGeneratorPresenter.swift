bc92b19809c16858c1bcc3f7071e8c1b517c16fd
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-07-28 17:49:58 -0600

    added presentation layer

---
+++ TestProject1/Presentation/UserGeneratorPresenter.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+protocol UserGeneratorPresenterProtocol {
+    func retrieveUserData()
+}
+
+protocol UserGeneratorViewProtocol {
+    func showError(_ error: String)
+    func showLoader()
+    func hideLoader()
+    func diplayUserData(_ userData: UserData)
+}
+
+struct UserGeneratorPresenter: UserGeneratorPresenterProtocol {
+    
+    private var ramdomUserProvider: RamdomUserProviderProtocol
+    private var userGeneratorView: UserGeneratorViewProtocol
+    let constants: Constants = Constants()
+    
+    init(ramdomUserProvider: RamdomUserProviderProtocol,
+         userGeneratorView: UserGeneratorViewProtocol) {
+        self.ramdomUserProvider = ramdomUserProvider
+        self.userGeneratorView = userGeneratorView
+    }
+    
+    func retrieveUserData() {
+        userGeneratorView.showLoader()
+        ramdomUserProvider.getRamdomUserProvider().execute { responseData in
+            userGeneratorView.hideLoader()
+            switch responseData {
+            case .success(let userData):
+                userGeneratorView.diplayUserData(userData)
+            case .failure(let error):
+                userGeneratorView.showError(error.localizedDescription)
+            }
+        }
+    }
+}
